6e9ee7c2d4779ad05af43fd9468547c59e378a79
Vamshhi Polapally <pvamshhi.official@gmail.com>
2026-01-23 22:23:58 +0530

    Add files via upload

---
+++ USDS/usds_2.0/17-01-2026/code.py
@@ -0,0 +1,19 @@
+a = 12
+b = 12
+c = 12.0
+
+# fsfsedfsdfs 
+print(id(a))
+id(b)
+id(c)
+
+'''
+This is Ultimate data science batch v2.0.
+In this class we are learning python variable
+This is Ultimate data science batch v2.0.
+In this class we are learning python variable
+This is Ultimate data science batch v2.0.
+In this class we are learning python variable
+print(a)
+'''
+print(a)
